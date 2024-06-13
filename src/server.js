a4af9453f99bea8d6ccf97eab9bde372649b9e87
Hiwotgebre <hiwotg173@gmail.com>
2024-06-13 18:08:10 -0400

    declaring a variable for Import routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT =process.env.PORT || 3000;
+const PORT = 3000;
 
 //Middleware to parase Json and url-encoded form data
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.unlencoded({ extended: true}));
+
+//Import routes
+const taskRoutes = require('./routes/taskRoutes');
