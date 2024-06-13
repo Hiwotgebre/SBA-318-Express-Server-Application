3a31c291dedeebc79ad2de9a400e443086001a30
Hiwotgebre <hiwotg173@gmail.com>
2024-06-13 18:22:48 -0400

    correcting an error on typo

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 //Middleware to parase Json and url-encoded form data
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
-app.use(express.unlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 
 //Import routes
 const taskRoutes = require('./routes/taskRoutes');
