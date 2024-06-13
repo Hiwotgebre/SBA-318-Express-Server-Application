4ab0db5849c5bd44a9e421c64041d675e5b28a8c
Hiwotgebre <hiwotg173@gmail.com>
2024-06-13 13:15:02 -0400

    creating a folder called src and a file called server.js

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT =process.env.PORT || 3000;
+
+//Middleware to parase Json and url-encoded form data
+app.use(express.json());
+app.use(express.unlencoded({extended: true}));
